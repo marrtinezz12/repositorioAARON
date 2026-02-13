ad27dcffa0382a298291f09c3de3cf9359c5ce1e
user <user@DESKTOP-B366R1N>
2026-02-13 13:56:21 +0100

    wertyu

--- PROYECTOREPOSITORIO1/src/paqueteRepositorio1/ClaseRepositorio1.java
+++ PROYECTOREPOSITORIO1/src/paqueteRepositorio1/ClaseRepositorio1.java
@@ -4,6 +4,6 @@ public class ClaseRepositorio1 {
 public static void main(String [] args) {
 	System.out.println("Pringui");
 	//Que miras
-	System.out.println("Ole ole los caracole");
+	System.out.println("Ole ole los caytracole");
 }
 }
