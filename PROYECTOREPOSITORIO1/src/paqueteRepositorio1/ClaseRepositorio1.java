bf2ff2ad09e34dc99165f964b45607e07838837a
ElMarrtinehh <ElMarrtinehh@DESKTOP-ENB86QN>
2026-02-13 13:26:37 +0100

    Ole que ole

--- PROYECTOREPOSITORIO1/src/paqueteRepositorio1/ClaseRepositorio1.java
+++ PROYECTOREPOSITORIO1/src/paqueteRepositorio1/ClaseRepositorio1.java
@@ -3,5 +3,7 @@ package paqueteRepositorio1;
 public class ClaseRepositorio1 {
 public static void main(String [] args) {
 	System.out.println("Pringui");
+	//Que miras
+	System.out.println("Ole ole los caracole");
 }
 }
